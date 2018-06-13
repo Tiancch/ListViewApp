1847caf362da8ddd2eac7a129c0373dcf02cc677
tiancch <tiancch@gmail.com>
2018-06-12 22:16:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/user/listviewapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.user.listviewapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    ListView mListView;
+    String[] countryNames = {"Australia","Brazil","Canada","India","Mexico","Portugal","USA"};
+    int[] countryFlags = {R.drawable.australia_flag, R.drawable.brazil_flag,R.drawable.canada_flag,R.drawable.india_flag,R.drawable.mexico_flag,R.drawable.portugal_flag,R.drawable.usa_flag};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(getResources().getString(R.string.app_name));
+        mListView = (ListView) findViewById(R.id.listview);
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this, countryNames, countryFlags);
+        mListView.setAdapter(myAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent mIntent = new Intent(MainActivity.this, DetailActivity.class);
+                mIntent.putExtra("countryName", countryNames[i]);
+                mIntent.putExtra("countryFlag", countryFlags[i]);
+                startActivity(mIntent);
+            }
+        });
+
+    }
+}
